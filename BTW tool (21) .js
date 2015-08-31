b584e9bbf79a2a096276750b939c7b71c08765f8
balderclaassen <balderclaassen@live.nl>
2015-08-31 02:47:53 +0200

    Update BTW tool (21) .js
    Our first testing today showed we needed to subtract instead of add the 21%. + Rounding to two decimals, now includes adding 1 or more extra zero's after the comma when necessary

--- BTW tool (21) .js
+++ BTW tool (21) .js
@@ -1,27 +1,32 @@
 var bedrag
+var BTW
+var MinBTW
 
 function herhalingBedrag() {
-    return "Dit is het bedrag: € " + bedrag;
+	return "Dit is het bedrag: € " + bedrag;
 };
 
 function alleenBTW() {
-    return "De btw (21%) van het bedrag: € " + Math.round((bedrag * 0.21)*100)/100;
+	BTW = (Math.round((bedrag * 0.21)*100)/100).toFixed(2); 	    
+	return "De btw (21%) van het bedrag: € " + BTW;
 };
 
-function bedragEnBTW() {
-    return "Het bedrag met BTW (21%): € " + Math.round((bedrag * 1.21)*100)/100;
+function bedragMinBTW() {
+	MinBTW = (bedrag - BTW);
+	return "Het bedrag min de BTW (21%): € " + (Math.round(MinBTW*100)/100).toFixed(2);
 };
 
 
 function berekenBTW() {
-  bedrag = prompt("Vul hier in, het bedrag waar van je de hoge BTW (21%) wilt weten. " + '\n' + '\n' + "(format: 0.00 en niet 0,00)" + '\n' + '\n' + "klik op cancel/anuleren en daarna ok, als je wilt stoppen. Klik op Run(op de achtergrond) als je bent gestopt maar weer wil beginnen.");
-    alert(herhalingBedrag() + '\n' + alleenBTW() + '\n' + bedragEnBTW());
+	bedrag = prompt("Vul hier in, het bedrag waar van je de hoge BTW (21%) wilt weten. " + '\n' + '\n' + "(gebruik punt '.' en niet komma',')" + '\n' + '\n' + "klik op cancel/anuleren en daarna ok, als je wilt stoppen. Klik op Run(op de achtergrond) als je bent gestopt maar weer wil beginnen.");
+	alert(herhalingBedrag() + '\n' + alleenBTW() + '\n' + bedragMinBTW());
 };
 
-do{
-    berekenBTW()
-    }
-while(bedrag!==null)
+do {
+	berekenBTW()
+}
+while(bedrag!==null);
 
-// hosted at http://jsfiddle.net/p5c465yg/5/
-// 6% hosted at http://jsfiddle.net/r4vLyaam/
+
+// 21% hosted at http://jsfiddle.net/p5c465yg/8/
+// 6% hosted at http://jsfiddle.net/r4vLyaam/1
