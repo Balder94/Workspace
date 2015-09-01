9ad13849307b2192c699b7b4edeff2d4d5353d13
balderclaassen <balderclaassen@live.nl>
2015-09-01 13:10:47 +0200

    Update BTW tool (21) .js

--- BTW tool (21) .js
+++ BTW tool (21) .js
@@ -1,26 +1,26 @@
 var bedrag
 var BTW
-var MinBTW
+var ExBTW
 
 function herhalingBedrag() {
-	return "Dit is het bedrag: € " + bedrag;
+	return "Dit is het bedrag(incl.): € " + bedrag;
+};
+
+function bedragExBTW() {
+	ExBTW = (bedrag/1.21).toFixed(2);
+	return "Het bedrag exclusief BTW: € " + (Math.round(ExBTW*100)/100).toFixed(2);
 };
 
 function alleenBTW() {
-	BTW = (Math.round((bedrag * 0.21)*100)/100).toFixed(2); 	    
-	return "De btw (21%) van het bedrag: € " + BTW;
+	BTW = (Math.round((ExBTW * 0.21)*100)/100).toFixed(2);
+	return "De BTW van het bedrag: € " + BTW;
 };
-
-function bedragMinBTW() {
-	MinBTW = (bedrag - BTW);
-	return "Het bedrag min de BTW (21%): € " + (Math.round(MinBTW*100)/100).toFixed(2);
-};
-
 
 function berekenBTW() {
-	bedrag = prompt("Vul hier in, het bedrag waar van je de hoge BTW (21%) wilt weten. " + '\n' + '\n' + "(gebruik punt '.' en niet komma',')" + '\n' + '\n' + "klik op cancel/anuleren en daarna ok, als je wilt stoppen. Klik op Run(op de achtergrond) als je bent gestopt maar weer wil beginnen.");
-	alert(herhalingBedrag() + '\n' + alleenBTW() + '\n' + bedragMinBTW());
+	bedrag = prompt("Vul hier in, het bedrag(inclusief) waar van je de hoge BTW (21%) wilt weten. " + '\n' + '\n' + "(gebruik punt '.' en niet komma',')" + '\n' + '\n' + "klik op cancel/anuleren en daarna ok, als je wilt stoppen. Klik op Run(op de achtergrond) als je bent gestopt maar weer wil beginnen.");
+	alert(herhalingBedrag() + '\n' + bedragExBTW() + '\n' + alleenBTW());
 };
+
 
 do {
 	berekenBTW()
@@ -28,5 +28,5 @@ do {
 while(bedrag!==null);
 
 
-// 21% hosted at http://jsfiddle.net/p5c465yg/8/
-// 6% hosted at http://jsfiddle.net/r4vLyaam/1
+// 21% hosted at http://jsfiddle.net/p5c465yg/9
+// 6% hosted at http://jsfiddle.net/r4vLyaam/2
