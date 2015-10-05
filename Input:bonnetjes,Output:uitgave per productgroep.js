13ed4093444fb7d5e4f2e893e0d6823d07f8778d
Balder Claassen <balderclaassen@live.nl>
2015-10-05 14:07:23 +0200

    Initial commit

---
+++ Input:bonnetjes,Output:uitgave per productgroep.js
@@ -0,0 +1,20 @@
+function date() {
+    var Date = prompt("Wat is de datum van dit bonnetje ? (dd-mm-jjjj)")
+}
+
+function products() {
+    var ProductGroup = prompt("Typ de productgroep")
+    var ProductPrice = prompt("Typ de prijs")
+
+    for (;ProductGroup != null && ProductPrice != null; i=i+1)
+}
+
+
+
+
+function input(Date,ProductGroup,ProductPrice) {
+var log = Date,ProductGroup,ProductPrice
+return(Date +","+ ProductGroup +","+ ProductPrice);
+console.log(log)
+}
+var = variable(1,"f",3);
