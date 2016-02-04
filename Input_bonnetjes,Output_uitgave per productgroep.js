a9c5bd96cde11400840e3eea48b7bf1b4ee5bf03
Balder Claassen <balderclaassen@live.nl>
2016-02-04 11:49:04 +0100

    Updated

---
+++ Input_bonnetjes,Output_uitgave per productgroep.js
@@ -0,0 +1,23 @@
+function date() {
+    var Date = prompt("Wat is de datum van dit bonnetje ? (dd-mm-jjjj)")
+}
+
+var i;
+function products() {
+    var ProductGroup = prompt("Typ de productgroep");
+    var ProductPrice = prompt("Typ de prijs");
+
+    for (;ProductGroup != null && ProductPrice != null; i=i+1);
+    //for loop condition crashes
+    return i ;
+};
+products()
+
+
+
+
+function input(Date,ProductGroup,ProductPrice) {
+var log = Date +"|"+ ProductGroup +"|"+"€"+ ProductPrice;
+return log;
+}
+input("21-10-2015","Vega","5,68")
---
+++ Input_bonnetjes.js
@@ -0,0 +1,5 @@
+function input(Date,ProductGroup,ProductPrice) {
+var log = Date +"|"+ ProductGroup +"|"+"€"+ ProductPrice;
+return log;
+}
+input("21-10-2015","Vega","5,68")
