084621d7e1cd7f4497e09f00ae74c538c362d0de
Balder Claassen <balderclaassen@live.nl>
2016-04-28 22:42:16 +0200

    fixed semicolons and whitespace

--- Opteller.js
+++ Opteller.js
@@ -1,45 +1,45 @@
-Option 1: 
-var calculation
+ //Option 1
 
-function RecieveInput() 
+var calculation;
+
+function RecieveInput()
 {
-	calculation = prompt("Typ hier de getallen die je bij elkaar wil optellen met een spatie tussen ieder getal.")
+	calculation = prompt("Typ hier de getallen die je bij elkaar wil optellen met een spatie tussen ieder getal.");
 }
 
 function MakeReadable()
 {
-    while (calculation.indexOf("  ") !== -1) 
-    { calculation = calculation.replace(/  /g, " ") };
+    while (calculation.indexOf("  ") !== -1)
+    {calculation = calculation.replace(/  /g, " ");}
     return calculation.split(" ");
     }
 
-function GiveOutput
-{
+function GiveOutput() {
    MakeReadable() * * * *  
 }
 
 
 function Opteller()
 {
-	Recieveinput()
-	
-	GiveOutput()
+	Recieveinput();
+
+	GiveOutput();
 }
 
-Opteller()
+Opteller();
 ///////////////////////////////////////////
 
-Option 2 : 
+// Option 2
 
-var Result
-var Input = prompt("Type in the numbers, seperated by spaces")
-alert("5".toFixed(2))
+var Result;
+var Input = prompt("Type in the numbers, seperated by spaces");
+alert("5".toFixed(2));
 
 for (Result=1; Result <100; Result += Input) {
-    
+
 }
 
-alert(Result)
+alert(Result);
 
 
 //things i want to search in google when internet comes back up
