bfc810cee410396a4591200321654349e0f5bdf9
Balder Claassen <balderclaassen@live.nl>
2016-04-28 22:44:52 +0200

    Intial commit

---
+++ Simpele Calculaties.js
@@ -0,0 +1,30 @@
+function omgekeerdewortel(input) {
+   var result = input*input;
+   console.log(result);
+}
+
+
+var input;
+
+function WortelBerekenaar(input) {
+   VarInput(9,3);
+   VarInput(16,4);
+   VarInput();
+
+   placeholder();
+   }
+
+
+
+
+function VarInput (var1,var2) {
+   if (input == var1) {
+      console.log(input/var2);
+   }
+}
+
+function placeholder () {
+   if ( input < 100) {
+         console.log();
+}
+}
